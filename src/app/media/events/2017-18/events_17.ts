71bb7f2feabaf3a95156f58f469ae919da2d8a72
Ravi Kumar <ravikr12082018@gmail.com>
2025-01-11 18:57:34 +0530

    Media Updated 2017-18 and 2018-19

--- src/app/media/events/2017-18/events_17.ts
+++ src/app/media/events/2017-18/events_17.ts
@@ -1,34 +1,209 @@
 export const events_17 = [
   {
-    name: "XYZ Event",
-    image: "/images/events/homepage-1.jpg",
+    name: "BOOKS DISTRIBUTION",
+    image: "/images/events/2017-18/2017-18-1.jpg",
     content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam tenetur quis facere quam rerum officiis dolore, quos, itaque vitae odit pariatur omnis atque. Voluptate, temporibus quia? Deleniti accusamus dignissimos hic?",
-    date: "12th Jan, 2025", // Add the date
-    instagram: "https://www.instagram.com/your_page", // Add Instagram link
+      "Books and stationery were distributed to underprivileged students by the VRIKSH TEAM. We would like to extend our heartfelt thanks to all the members of VRIKSH BE THE CHANGE for their generous financial and moral support.",
+    quote: "\"One Child, One Teacher, One Pen And One Book Can Change The World.\"",
+    date: "27th April, 2017", // Add the date
+    instagram: "#", // Add Instagram link
   },
   {
-    name: "XYZ Event",
-    image: "/images/events/homepage-1.jpg",
+    name: "Motivation by Pitamber Kumar (Income Tax Officer)",
+    image: "/images/events/2017-18/2017-18-2.jpg",
     content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam tenetur quis facere quam rerum officiis dolore, quos, itaque vitae odit pariatur omnis atque. Voluptate, temporibus quia? Deleniti accusamus dignissimos hic?",
-    date: "15th Feb, 2025",
-    instagram: "https://www.instagram.com/your_page",
+      "Motivation is important because it provides the drive for individuals to accomplish their goals, fulfill their responsibilities, and solve problems. He shared valuable advice on how to focus on studies and set life goals. He also spoke about his own hard work in achieving his objectives.",
+    quote: "\"Success is not final, failure is not fatal: it is the courage to continue that counts.\"",
+    date: "5th May, 2017", // Add the date
+    instagram: "#", // Add Instagram link
   },
   {
-    name: "XYZ Event",
-    image: "/images/events/homepage-1.jpg",
+    name: "Visiting Unique English School",
+    image: "/images/events/2017-18/2017-18-3.jpg",
     content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam tenetur quis facere quam rerum officiis dolore, quos, itaque vitae odit pariatur omnis atque. Voluptate, temporibus quia? Deleniti accusamus dignissimos hic?",
-    date: "20th Mar, 2025",
-    instagram: "https://www.instagram.com/your_page",
+      "The VRIKSH TEAM was invited by UNIQUE ENGLISH SCHOOL to motivate the students toward achieving their goals. We had a golden opportunity to interact with the students and raise awareness about the importance of value-added education and time management.",
+    quote: "\"Telling children about the bad things happening around the world is important, but telling them about the good that has been done and can be done is even more crucial in shaping a bright future.\"",
+    date: "5th May, 2017", // Add the date
+    instagram: "#", // Add Instagram link
   },
   {
-    name: "XYZ Event",
-    image: "/images/events/homepage-1.jpg",
+    name: "MOTHER\'S DAY 2017",
+    image: "/images/events/2017-18/2017-18-4.jpg",
     content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam tenetur quis facere quam rerum officiis dolore, quos, itaque vitae odit pariatur omnis atque. Voluptate, temporibus quia? Deleniti accusamus dignissimos hic?",
-    date: "5th Apr, 2025",
-    instagram: "https://www.instagram.com/your_page",
+      "Mother's Day was celebrated by the VRIKSH TEAM with students and their parents. Mother\'s Day, also known as Mothering Sunday, is celebrated on the second Sunday of May, which falls on the 14th this year. Many inspiring stories were shared among them.",
+    quote: "\"Mothers hold their children's hands for a short while, but their hearts forever.\"",
+    date: "14th May, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "CLOTHES DISTRIBUTION",
+    image: "/images/events/2017-18/2017-18-5.jpg",
+    content:
+      "We also collected clothes from the local community and donated it to needy children. Our clothes donation drive is only for the children who have not enough clothing to cover the body. We would like to thank the families who donated clothes for these deprived children. We collect various kinds of summer and winter clothes for children as well as adults at our office, HOME CENTRE, Manpur Patwatoli, Gaya, Bihar throughout the year. A small thing from you would be gold for the living poor, with which they shall be able to bear the harshest scorching winds of summer or biting gusts of winter. Exhibit your generosity through us and help the maximum number of clothes be collected; let's manage to give adequate warmth and coziness to everybody.",
+    quote: "\"No man ever became poor by giving.\"",
+    date: "16th May, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "Registration VRIKSH BE THE CHANGE as TRUST",
+    image: "/images/events/2017-18/2017-18-6.jpg",
+    content:
+      "VRIKSH BE THE CHANGE is a group established in 2013 in Manpur Patwatoli, a village famous for its IITian residents, and registered under the Indian Trust Act 1882 in 2017. It is a group of people who provide selfless service to society in the fields of education, healthcare, community development, and the environment with an aim towards sustainable development of our society.",
+    quote: "\"The best way to find yourself is to lose yourself in the service of others.\"",
+    date: "26th May, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "OFFICE SET UP",
+    image: "/images/events/2017-18/2017-18-7.jpg",
+    content:
+      "An office was set up to ensure the smooth operation of VRIKSH BE THE CHANGE, which is registered under the Indian Trust Act 1882. We are deeply grateful to the MRJR family, who reside in Manpur Patwatoli, for kindly providing us with the space for our office.",
+    quote: "\"Alone we can do so little; together we can do so much.\"",
+    date: "27th May, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "Motivation By MOTILAL SIR and JITENDRA PD",
+    image: "/images/events/2017-18/2017-18-8.jpg",
+    content:
+      "We thank Motilal Sir and Jitendra Pd. for visiting VRIKSH PATHSHALA and motivating our students and members to work for society. They appreciated the efforts of the VRIKSH TEAM and promised all sorts of support- both monetary and moral. They even shared their own experiences of difficulties that they have encountered in pursuit of education.",
+    quote: "\"Education is one of the many blessings of life, and without money, those blessings cannot stop.\"",
+    date: "27th June, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "NAVPRAYAS PRATIBHA MILAN 2017",
+    image: "/images/events/2017-18/2017-18-9.jpg",
+    content:
+      "NAVPRAYAS (Group of Innovative Thoughts) is a group of engineers based in Manpur Patwatoli, Gaya, Bihar. VRIKSH BE THE CHANGE was honored at PRATIBHA MILAN 2017 for its selfless service to society. The speech was delivered by the Founder of VRIKSH BE THE CHANGE, Chandrakant Pateshwari, and our President, Dugeshwar Prasad.",
+    quote: "\"As we lose ourselves in the service of others, we discover our true purpose.\"",
+    date: "3rd July, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "DUSSEHRA CELEBRATION AND CLOTHES DISTRIBUTION",
+    image: "/images/events/2017-18/2017-18-10.jpg",
+    content:
+      "On the occasion of Dussehra, Kuldeep Kumar (a member of VRIKSH BE THE CHANGE) and his family distributed new clothes to the female students. \"Providing gifts and clothes to children\" is an opportunity that allows them to relax, share gifts, exchange experiences, and tell stories in a delightful new environment outside their homes, even if just for one day with the members. These clothes will become a golden memory for the children.",
+    quote: "\"Everything we do for these kids now will never be erased from their memories.\"",
+    date: "25th Sep, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "TEACHER\'S DAY",
+    image: "/images/events/2017-18/2017-18-11.jpg",
+    content:
+      "Teacher's Day was celebrated by the students and members of VRIKSH BE THE CHANGE. Teachers mold students, like wet clay, into future citizens who are morally upright, responsible, and good human beings. In honor of their teachers, students planned a formal get-together on the eve of Teacher's Day, with the students acting as hosts. Every class contributed and created a small gift hamper for their respective teachers. This event was celebrated in the day when students performed dances, delivered speeches, and did many things that made it a fun celebration day.",
+    quote: "\"A teacher is a compass that activates the magnets of curiosity, knowledge, and wisdom in the pupils.\"",
+    date: "5th Sep, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "DIWALI CELEBRATION",
+    image: "/images/events/2017-18/2017-18-12.jpg",
+    content:
+      "Following the tradition of \"Tumso Ma Jyotirgamaye,\" the Festival of Lights was hosted with genuine efforts to lighten the lives of poor children who still find reasons to smile in their difficult conditions. Like all the functions and festivals, the children were taught the origins of the Festival of Lights, the meaning, and its significance. The festival itself is to move from the darkness into the light: sharing and spreading that light of wisdom, love, peace, and prosperity.",
+    quote: "\"Let this DIWALI burn all your bad times and enter you in good times.\"",
+    date: "19th Oct, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "CHILDREN\'S DAY",
+    image: "/images/events/2017-18/2017-18-13.jpg",
+    content:
+      "Children's Day was celebrated on the 14th of November by bringing light to the underprivileged children's lives. As this day calls for importance due to children, we can definitely make it special and bring happiness in others' lives. The students on this day felt thrilled. Various fun games have been organized by the VRIKSH TEAM to make it an interesting activity. Prizes were given to the students for supporting VRIKSH PATHSHALA consistently for dedicating valuable time.",
+    quote: "\"VRIKSH BE THE CHANGE believes that the more happiness you spread the happier you get.\"",
+    date: "14th Nov, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "MOTIVATION BY PAWAN KUMAR (USA)",
+    image: "/images/events/2017-18/2017-18-14.jpg",
+    content:
+      "It was a very proud moment for VRIKSH BE THE CHANGE when Mr. Pawan Kumar visited VRIKSH PATHSHALA to motivate the students and members. He is currently working at one of the best software companies, Microsoft, in the USA. He shared insights about the American system of education and appreciated the work being done by our team. He also committed to supporting VRIKSH BE THE CHANGE both financially and morally.",
+    quote: "\"There is only one thing that makes a dream impossible to achieve: the fear of failure.\"",
+    date: "29th Dec, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "PARENTS MEETING",
+    image: "/images/events/2017-18/2017-18-15.jpg",
+    content:
+      "A meeting for parents was arranged on 30th November, 2017 at Manpur Patwatoli at 4:00 PM. At the meeting, parents were informed about their role and responsibilities and guided to protect their children from the significant social evils. VRIKSH BE THE CHANGE focuses on enhancing parental involvement in the education of their children.",
+    quote: "\"The best inheritance a parent can give to his children is a few minutes of their time each day.\"",
+    date: "30th Nov, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "MOTIVATIONAL SEMINAR",
+    image: "/images/events/2017-18/2017-18-16.jpg",
+    content:
+      "VRIKSH BE THE CHANGE invited SURYADEO SINGH and MOTILAL SIR to motivate the VRIKSH PATHSHALA STUDENTS. A motivational speaker or inspirational speaker is a speaker who makes speeches intended to motivate or inspire an audience.",
+    quote: "\"If you don't design your own life plan, chances are you'll fall into someone else's plan. And guess what they have planned for you? Not much.\"",
+    date: "23rd Dec, 2017", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "MANAV SHRINKHALA IN BIHAR",
+    image: "/images/events/2017-18/2017-18-17.jpg",
+    content:
+      "Rangoli was created by VRIKSH students for the \"MANAV SHRINKHALA,\" an initiative suggested by the Bihar Government on 21st January 2018 to combat child marriage and the dowry system in Bihar. VRIKSH BE THE CHANGE always works for a cause, not for applause. VRIKSH members actively participated in this human chain to bring about change. We must do our utmost to put an end to the harmful custom of child marriage. \"Educating girls is one of the most powerful tools to prevent child marriage.\"",
+    quote: "\"Any young man, who makes dowry a condition to marriage, discredits his education and his country and dishonours womanhood.\"",
+    date: "21th Jan, 2018", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "SARASWATI PUJA",
+    image: "/images/events/2017-18/2017-18-18.jpg",
+    content:
+      "VRIKSH BE THE CHANGE students celebrated SARASWATI PUJA at Home Centre. The puja is performed to show allegiance to the Goddess of learning. Goddess SARASWATI is worshipped as an elegant woman wearing white, symbolic of purity and peace. It is the goddess who symbolises knowledge, learning, arts and culture.",
+    quote: "\"Our Heartiest good wishes to all members of your family on the auspicious day of Saraswati Puja. May Lord Saraswati shower blessings.\"",
+    date: "22nd Jan, 2018", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "Workshop on Child Sexual Abuse",
+    image: "/images/events/2017-18/2017-18-19.jpg",
+    content:
+      "VRIKSH BE THE CHANGE organized a workshop on Child Sexual Abuse on 04-03-2018 at 4:00 PM at Nirankari Satsang Bhavan, Manpur. We extend our thanks to the Nirankari Mission for providing the space for this workshop. Videos were shown to educate parents on how to protect their children from such abuse. Guest speakers delivered lectures to raise awareness among parents about the dangers of child sexual abuse and the steps they can take to prevent it.",
+    quote: "\"There are instances in which \"Child Sexual Abuse\" can be clearly connected to a collective failure of society.\"",
+    date: "4th March, 2018", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "Knowledge Sharing About DR. B. R. Ambedkar",
+    image: "/images/events/2017-18/2017-18-20.jpg",
+    content:
+      "B. R. Ambedkar. Bhimrao Ramji Ambedkar (14 April 1891 – 6 December 1956), popularly known as Baba Saheb, was an Indian jurist, economist, politician and social reformer who inspired the Dalit Buddhist Movement and campaigned against social discrimination against Untouchables (Dalits), while also supporting the rights of women and labour. He was Independent India's first law minister, the principal architect of the Constitution of India and a founding father of the Republic of India. Lalan Ji, Social worker and follower of Ambedkarism shared his knowledge about BABA SAHEB among the students and VRIKSH TEAM.",
+    quote: "\"I measure the progress of a community by the degree of progress which women have achieved.\" ― B.R. Ambedkar",
+    date: "17th March, 2018", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "MUSICAL CONCERT",
+    image: "/images/events/2017-18/2017-18-21.jpg",
+    content:
+      "A musical concert was organized on 25th March 2018 at Nirankari Satsang Bhawan, Manpur, at 4:00 PM. Working together in concert not only helps us move more efficiently but also enhances the scope of what we can achieve. When we have the confidence to orchestrate a collective effort, we dare to dream bigger. We would like to extend our gratitude to Ganesh Kala Niketan and Shankar Kala Niketan for their efforts in organizing this event. \"Coming together is a beginning; keeping together is progress; working together is success.\"",
+    quote: "\"Music is the universal language of mankind.\"",
+    date: "25th March, 2018", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: " PATHSHALA 2017",
+    image: "/images/events/2017-18/2017-18-22.jpg",
+    content:
+      "PATHSHALA TEAM is working selflessly to educate the underprivileged students. There are 178 students getting help from VRIKSH BE THE CHANGE financially and morally. Now the venue of the classes has been changed to BRILLIANT ENGLISH SCHOOL. We would like to thank the Director of this School, Anil Sir, for his support by giving the school space to the VRIKSH BE THE CHANGE. We are growing year by year. This is only possible by the support of VRIKSH TEAM, VRIKSH MEMBERS, STUDENTS, SCHOOLS, and LOCAL PEOPLE.",
+    quote: "\"YOU KEEP SUPPORTING FINANCIALLY AND MORALLY WE KEEP GOING AND GROWING PERIODICALLY.\"",
+    date: "-", // Add the date
+    instagram: "#", // Add Instagram link
+  },
+  {
+    name: "TEAM WORK 2017",
+    image: "/images/events/2017-18/2017-18-23.jpg",
+    content:
+      "Teamwork is a team's joint effort to attain a common goal or to do a task in the most efficient and effective way. VRIKSH TEAM understands the goals and is committed to attaining them. This clear direction and agreement on mission and purpose is vital for effective teamwork. The general objective of a team is that which is agreed upon and provides the umbrella for all that the team tries to do. Such team clarity is reinforced as when the VRIKSH BE THE CHANGE has clear expectations on work, goals, accountability, and outcomes for the team.",
+    quote: "\"The strength of the team is each individual member. The strength of each member is the team.\"",
+    date: "-", // Add the date
+    instagram: "#", // Add Instagram link
   },
 ];
--- src/app/media/events/2018-19/page.tsx
+++ src/app/media/events/2018-19/page.tsx
@@ -5,7 +5,7 @@ const Page = () => {
   return (
     <div className="w-full flex flex-wrap gap-4 justify-center p-4 flex-col">
       <div className="font-bold flex text-center text-xl gap-4 items-center justify-center">
-        Events of the year 2018
+        Events of the year 2018-19
       </div>
       <div className="flex gap-4 items-center justify-center flex-wrap">
         {events_18.map((e, idx) => {
@@ -14,6 +14,7 @@ const Page = () => {
               name={e.name}
               image={e.image}
               content={e.content}
+              quote={e.quote}
               date={e.date}
               instagram={e.instagram}
               key={idx}
