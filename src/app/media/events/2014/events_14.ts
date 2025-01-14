b67da18de7d0a7e1db8eb766951fad6a0af011cc
manish58bit <manish70919@gmail.com>
2025-01-14 14:56:53 +0530

    media content updated

--- src/app/media/events/2014/events_14.ts
+++ src/app/media/events/2014/events_14.ts
@@ -1,5 +1,6 @@
 export const events_14 = [
   {
+    name: "Parent-Student meeting",
     image: "/images/events/2014/2014-2.png",
     content:
       'On 18 October 2014, VRIKSH BE THE CHANGE had organized a Parent-Student meeting at 3:30 pm in Manpur Patwatoli. The purpose of the meeting was to have a common platform where the VRIKSH team and parents could come together to enrich the educational experiences of the students and discuss various issues concerning the holistic development of the students. The meeting was conducted under the guidance of Chandrakant Pateshwari and Dugeshwar Prasad. <br/ ><br/> <b><i>"Coming together is a beginning; keeping together is progress; working together is success."<i/><b/> – Lao Tzu',
--- src/app/media/events/2015/events_15.ts
+++ src/app/media/events/2015/events_15.ts
@@ -1,6 +1,6 @@
 export const events_15 = [
   {
-    name: "",
+    name: "VRIKSH BE THE CHANGE initiated a project called PATHSHALA",
     image: "/images/events/2015/2015-2.png",
     content:
       'Under the Education chapter, VRIKSH BE THE CHANGE initiated a project called PATHSHALA in 2015. The main goal of PATHSHALA is to provide quality school education to underprivileged children in India so that they can complete Class X, regardless of the challenges faced by their family, and grow into socially responsible, independent, and mature adults.PATHSHALA supports financially and morally, and it also calls the students to the study center daily at 4:00 PM for an hour of focused study. Its objective is regular academic support. The team-includes teachers, senior students, VRIKSH members, and trustees-does its business of meetings daily for one hour. Matters related to present situations and future activities about PATHSHALA are discussed regularly.<br/ ><br/> <b><i>"In learning you will teach, and in teaching you will learn."<i/><b/>',
@@ -8,7 +8,7 @@ export const events_15 = [
     instagram: "https://www.instagram.com/your_page", // Add Instagram link
   },
   {
-    name: "",
+    name: "VRIKSH organized a Parent-Student meeting on Children's Day",
     image: "/images/events/2015/2015-1.png",
     content:
       'On November 14, 2015, VRIKSH BE THE CHANGE organized a Parent-Student meeting on Children\'s Day at 10:00 AM in Manpur Patwatoli. The primary objective of the meeting was to motivate both students and parents. Distinguished guests shared their personal stories of overcoming challenges to attain education and engaged in discussions on the holistic development of students.<br/ ><br/> <b><i>"Success is the sum of small efforts, repeated day in and day out."<i/><b/>',
